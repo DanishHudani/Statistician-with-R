c09d14b4866520a8125cd12ad0bf8f96ae453c0d
DanishHudani <danish.hudani78@gmail.com>
2021-03-22 01:46:40 +0500

    Central Limit Theorm

--- R-Scripts/1_Introduction_to_Statistics_in_R/3_More_Distributions_and_the_Central_Limit_Theorem.R
+++ R-Scripts/1_Introduction_to_Statistics_in_R/3_More_Distributions_and_the_Central_Limit_Theorem.R
@@ -37,7 +37,40 @@ new_sales <- new_sales %>% mutate(amount = rnorm(36, new_mean, new_sd))
 ggplot(new_sales, aes(x = amount)) + geom_histogram(bins = 10)
 
 
+# Central Limit Theorm
 
+# Create a histogram of num_users
+ggplot(amir_deals, aes(x = num_users)) + geom_histogram(bins = 10)
+
+# Set seed to 104
+set.seed(104)
+
+# Sample 20 num_users with replacement from amir_deals
+sample(amir_deals$num_users, 20, replace = TRUE) %>%
+  # Take mean
+  mean()
+
+sample_means <- replicate(100, sample(amir_deals$num_users, 20, replace = TRUE) %>% mean())
+sample_means
+
+# Create data frame for plotting
+samples <- data.frame(mean = sample_means)
+
+# Histogram of sample means
+ggplot(samples, aes(x = mean)) +
+  geom_histogram(bins = 10)
+
+# Set seed to 321
+set.seed(321)
+
+# Take 30 samples of 20 values of num_users, take mean of each sample
+sample_means <- replicate(30, sample(all_deals$num_users, 20) %>% mean())
+
+# Calculate mean of sample_means
+mean(sample_means)
+
+# Calculate mean of num_users in amir_deals
+mean(amir_deals$num_users)
 
 
 
